ccfb81dd450bf879febd3f46d422dd21b034c1e9
ctogan <ogan@cashtree.id>
2020-05-13 17:58:05 +0700

    from server

--- public/js/app.js
+++ public/js/app.js
@@ -37393,8 +37393,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/teskemampuan/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Applications/XAMPP/xamppfiles/htdocs/teskemampuan/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/teskemam/public_html/tk/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/teskemam/public_html/tk/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
